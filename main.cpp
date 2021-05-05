d674bd5d7410212044ae44a09b75012a8262db35
unknown <asmitjoy17@gmail.com>
2021-05-04 18:06:25 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // single line comment
+    /* Multiple line comment */
+    // cout<<"hello world"<<endl;
+    // cout<<"hi";
+
+    // int x, y, z;
+    // cin >> x;
+    // cin >> y;
+    // z = x + y;
+    // cout << "SOMA = " << z << endl;
+    for(int i = 0; i<20;i++){
+        cout<< "The value is "<<i<<endl;
+    }
+
+
+    return 0;
+
+}
