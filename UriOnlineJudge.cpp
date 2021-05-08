0f43690ad6be2ddbe80d13dc0e329b089858036d
unknown <asmitjoy17@gmail.com>
2021-05-08 13:46:41 -0800

    update

--- UriOnlineJudge.cpp
+++ UriOnlineJudge.cpp
@@ -12,12 +12,23 @@ int main() {
     // cout<<"X = "<<z;
 
 //    ************* 1002 ***********
-        double x,y,n=3.14159;
-        cin>>x;
-        y = x*x;
-        y = y*n;
-        cout<<"A="<<setprecision(4)<<fixed<<y<<endl;
+        // double x,y,n=3.14159;
+        // cin>>x;
+        // y = x*x;
+        // y = y*n;
+        // cout<<"A="<<setprecision(4)<<fixed<<y<<endl;
     
- 
+    // double t,a,b,c,x1,x2;
+    // cin>>a>>b>>c;
+    // if(((b*b)-4*a*c)<0 ||a==0){
+    //     cout<<"Impossivel calcular"<<endl;
+    // }else{
+    //      x1=((-b + sqrt((b*b)-4*a*c)) / (2 * a));
+    //      x2=((-b - sqrt((b*b)-4*a*c)) / (2 * a));
+    //      cout<<fixed;
+    //      cout<<setprecision(5)<<"R1 = "<<x1<<endl;
+    //      cout<<setprecision(5)<<"R2 = "<<x2<<endl;
+
+    // }
     return 0;
 }
